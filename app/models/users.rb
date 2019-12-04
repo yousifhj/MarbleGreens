c42f0fb71324da521d3678e86a035604b6734580
Huda Yousif <hjoski@gmail.com>
2019-12-04 15:27:37 -0800

    built user model

--- app/models/users.rb
+++ app/models/users.rb
@@ -1,4 +1,14 @@
 class Users < ActiveRecord::Base
     has_secure_password
     has_many :plants
+
+
+    def slug 
+        user.downcase.gsub(" ", "-")
+    end 
+
+    def self.find_by_slug(slug)
+        USer.all.find{|user| user.slug == slug}
+    end 
+
 end 
