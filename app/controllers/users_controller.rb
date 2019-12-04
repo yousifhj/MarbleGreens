11d25a6b1099c90f103924836fd1fc1f623c715c
Huda Yousif <hjoski@gmail.com>
2019-12-04 15:56:46 -0800

    update welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,21 @@
 class UsersController < ApplicationController
 
-get '/signup' do
-    if !logged_in?
-        erb :"users/signup"
-    else 
-        redirect "/plants"
+    get '/signup' do
+        if !logged_in?
+            erb :"users/signup"
+        else 
+            redirect "/plants"
+        end 
     end 
-  end 
+
+    post '/signup' do 
+        if params[:username] == "" && params[:password] == ""
+            redirect '/signup'
+        else 
+            @user = User.create(username: params[:username], password: params[:password])
+            sessions[:user_id] = @user.id
+            redirect "/plants"
+        end 
+    end 
 
 end 
