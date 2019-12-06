0afe167998b1aec82e14698c9dee7d096532fed9
Huda Yousif <hjoski@gmail.com>
2019-12-06 12:48:30 -0800

    update login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,13 +8,13 @@ class UsersController < ApplicationController
         end 
     end 
  
-    post '/signup' do
-        @user = User.new(username: params[:username], email: params[:email], password: params[:password])
-        if @user.save
-          session[:user_id] = @user.id
-          redirect '/plants'
-        else
-          erb :'/plants/index'
+    post "/signup" do
+        if params[:username] == "" || params[:password] == "" || params[:email] == ""
+            redirect to "/signup"
+        else 
+            user = User.create(:username => params[:username], :email => params[:email], :password => params[:password])
+            session[:user_id] = user.id
+            redirect "/plants"
         end
     end
 
@@ -27,18 +27,18 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        user = User.find_by(username: params[:username])
+        user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
           session[:user_id] = user.id
           redirect '/plants'
         else
-          erb :'/users/login'
+          redirect '/login'
         end
       end
 
     get '/logout' do
       session.clear
-      erb :'/index'
+      erb :'/homepage'
     end
 
 end 
