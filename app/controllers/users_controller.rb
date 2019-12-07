418f1eaec33c604e1ec090e849c4cefa17637fa5
Huda Yousif <hjoski@gmail.com>
2019-12-06 23:50:48 -0800

    remove plant and greenhouse from homepage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,7 @@
 class UsersController < ApplicationController
 
     get '/signup' do
-        if !logged_in?
             erb :"users/signup"
-        else 
-            redirect "/"
-        end 
     end 
  
     post "/signup" do
