e0c0136e29ab3c4abf4d29ce770df4f9bbeee6de
Huda Yousif <hjoski@gmail.com>
2019-12-09 23:00:58 -0800

    removed unnessary code

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,5 @@ class User < ActiveRecord::Base
     has_secure_password
     validates_presence_of :username, :email
     has_many :plants
-    
-
-    def slug 
-        user.downcase.gsub(" ", "-")
-    end 
-
-    def self.find_by_slug(slug)
-        USer.all.find{|user| user.slug == slug}
-    end 
 
 end 
